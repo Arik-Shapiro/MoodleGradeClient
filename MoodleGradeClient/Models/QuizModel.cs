247de9cb90670c411b8b5dca60dcc005ea81ce97
Arik Shapiro <arikshaapiro@gmail.com>
2021-07-12 20:41:11 +0300

    remove redundant quiz model fields
    Grade is sometimes double but it is not needed anymore because it's in the attempt.

--- MoodleGradeClient/Models/QuizModel.cs
+++ MoodleGradeClient/Models/QuizModel.cs
@@ -14,10 +14,6 @@ namespace MoodleGradeClient
         public int Course { get; set; }
         [JsonProperty("coursemodule", Required = Required.Always)]
         public int CourseModule { get; set; }
-        [JsonProperty("grade", Required = Required.Always)]
-        public int Grade { get; set; }
-        [JsonProperty("sumgrades")]
-        public double SumGrades { get; set; }
 
         [JsonProperty("name", Required = Required.Always)]
         public string Name { get; internal set; }
